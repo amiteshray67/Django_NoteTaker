9fa1d070e5d1131463dbca9208cf8aff03d519cd
Amitesh Ray <83110785+amiteshray67@users.noreply.github.com>
2022-05-02 19:18:52 +0530

    the first time

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,6 +26,8 @@ SECRET_KEY = 'django-insecure-_ld3+jqx@(ng#de!esw7u2_=645&*$zn29pi2ti_4$evh=^f_j
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+ALLOWED_HOSTS=['*']
 
 ALLOWED_HOSTS = []
 
@@ -124,10 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / 'static',
-]
-
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
