819fc7503400d006baafd5d00f26b5fb400f3346
Amitesh Ray <83110785+amiteshray67@users.noreply.github.com>
2022-05-04 18:28:33 +0530

    db changes

--- smartnotes/settings.py
+++ smartnotes/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
-django_heroku.settings(locals())
+STATICFILES_DIRS = [ 
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
